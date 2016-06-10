1d1ccf83ac6a20502f59c1f7c4c91e2343a96173
Catethysis <catethysis@gmail.com>
2016-06-10 03:04:51 +0300

    First commit: npm and gulp modules

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const browserify = require('browserify');
+const uglify = require('gulp-uglify');
+const source = require('vinyl-source-stream');
+const streamify = require('gulp-streamify');
+const rename = require("gulp-rename");
+const babelify = require('babelify');
+
+gulp.task('default', () => {
+	browserify('app.js', { debug: true })
+	.transform("babelify", {presets: ["es2015"]})
+	.bundle()
+	.pipe(source('bundle.js'))
+	.pipe(streamify(uglify()))
+	.pipe(rename('stm32.js'))
+	.pipe(gulp.dest('./'));
+});
